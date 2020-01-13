3e92548f5eac9e959f5dd96c3c0560a4edbe1ed8
Steven Zuniga <szuniga@cnm.edu>
2020-01-13 16:32:19 -0700

    Added formatted output.

--- src/edu/cnm/deepdive/CommandLine.java
+++ src/edu/cnm/deepdive/CommandLine.java
@@ -10,9 +10,9 @@ public class CommandLine {
 
   public static void main(String[] args) {
     Double[] data = read();
-    System.out.println( Arrays.toString(data));
+    System.out.printf("Input values: %s%n", Arrays.toString(data));
     double total = sum(data);
-    System.out.println( total );
+    System.out.printf( "Sum: %,.2f%n",total );
   }
 
   private static int[] parse(String[] args) {
